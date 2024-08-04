4a7c948f7e67959953b1384fe91f3071d09c3c84
Jeremy Marquis <jeremy@jeremymarquis.com>
2024-08-04 17:19:46 -0400

    cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,16 +7,8 @@ class ApplicationController < ActionController::Base
       game_list = Game.filter_valid_game_slugs([params[:game]])
     elsif params[:games].present?
       game_list = Game.filter_valid_game_slugs(params[:games].split(','))
-    elsif params[:melee].present? || params[:ultimate].present?
-      # TODO: Remove this eventually
-      games = [params[:melee] && Game::MELEE.slug, params[:ultimate] && Game::ULTIMATE.slug].filter(&:present?)
-      return redirect_to CGI::unescape(games.count > 1 ? root_path(games: games.join(',')) : root_path(game: games.first))
     elsif cookies[:games].present?
       game_list = Game.filter_valid_game_slugs(cookies[:games].split(','))
-    elsif cookies[:melee].present? || cookies[:ultimate].present?
-      # TODO: Remove this eventually
-      games = [cookies[:melee] && Game::MELEE.slug, cookies[:ultimate] && Game::ULTIMATE.slug].filter(&:present?)
-      return redirect_to CGI::unescape(games.count > 1 ? root_path(games: games.join(',')) : root_path(game: games.first))
     end
 
     game_list = [Game::MELEE.slug, Game::ULTIMATE.slug] if game_list.blank?
