b702a718fff271e638bcea5e7e8af522da71c84b
Jeremy Marquis <jeremy@jeremymarquis.com>
2024-10-23 20:22:07 -0400

    discord bot placeholder

---
+++ config/initializers/discord.rb
@@ -0,0 +1,9 @@
+# Thread.new do
+#   bot = Discordrb::Bot.new token: Rails.application.credentials.dig(:discord, :token)
+#
+#   bot.message(with_text: 'Ping!') do |event|
+#     event.respond 'Pong!'
+#   end
+#
+#   bot.run
+# end
