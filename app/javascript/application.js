566314e34cb7831d29aa5d9b822000ef8372a167
Jeremy Marquis <jeremy@jeremymarquis.com>
2024-10-26 17:31:35 -0400

    event start times

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,5 @@
 window.addEventListener("DOMContentLoaded", () => {
+
   const toggle = document.querySelector("#menu-toggle")
   const menuContainer = document.querySelector(".menu-container")
   if (menuContainer) {
@@ -7,4 +8,11 @@ window.addEventListener("DOMContentLoaded", () => {
       toggle.checked = false
     })
   }
+
+  const startTimes = document.querySelectorAll(".event-time")
+  startTimes.forEach(startTime => {
+    const time = moment.unix(startTime.getAttribute("datetime")).tz(Intl.DateTimeFormat().resolvedOptions().timeZone)
+    startTime.innerHTML = `Starting ${time.format("dddd")} @ ${time.format("h:mm a z")}`
+  })
+
 });
