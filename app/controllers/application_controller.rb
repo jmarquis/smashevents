0f7020a78db7cf5a0c6788943d4ac8da0e34d02c
Jeremy Marquis <jeremy@jeremymarquis.com>
2024-11-03 11:34:34 -0500

    player filtering

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,25 +16,28 @@ class ApplicationController < ActionController::Base
     @games = game_slugs.map { |slug| Game.find_by(slug:) }
     @unselected_games = Game.all_games_except(@games)
 
-    @tournaments = Rails.cache.fetch("tournaments_#{game_slugs.join('-')}", expires_in: Rails.env.development? ? 5.seconds : 5.minutes) do
-      Tournament
-        .includes(:override, events: :game)
-        # Leave a few hours of leeway for events that run long
-        .where('end_at > ?', Time.now - 6.hours)
-        .where(events: { game: @games })
-        .merge(
-          Tournament.where(override: { include: true }).or(
-            Tournament.where.not(events: { player_count: nil }).merge(
-              Tournament.where('coalesce(events.ranked_player_count, 0) / case when coalesce(events.player_count, 1) = 0 then 1 else coalesce(events.player_count, 1) end > ?', 0.3).or(
-                Tournament.where('events.ranked_player_count > ?', 10)
-              ).or(
-                Tournament.where('coalesce(events.player_count, 0) + (coalesce(events.ranked_player_count, 0) * 10) > games.display_threshold')
-              )
+    @tournaments = Tournament
+      .includes(:override, events: [:game, entrants: :player])
+      # Leave a few hours of leeway for events that run long
+      .where('end_at > ?', Time.now - 6.hours)
+      .where(events: { game: @games })
+      .merge(
+        Tournament.where(override: { include: true }).or(
+          Tournament.where.not(events: { player_count: nil }).merge(
+            Tournament.where('coalesce(events.ranked_player_count, 0) / case when coalesce(events.player_count, 1) = 0 then 1 else coalesce(events.player_count, 1) end > ?', 0.3).or(
+              Tournament.where('events.ranked_player_count > ?', 10)
+            ).or(
+              Tournament.where('coalesce(events.player_count, 0) + (coalesce(events.ranked_player_count, 0) * 10) > games.display_threshold')
             )
           )
         )
-        .order(start_at: :asc, end_at: :asc, name: :asc)
+      )
+
+    if params[:player]
+      @tournaments = @tournaments.where('LOWER(players.tag) = ?', params[:player].downcase)
     end
+
+    @tournaments = @tournaments.order(start_at: :asc, end_at: :asc, name: :asc)
 
   end
 
