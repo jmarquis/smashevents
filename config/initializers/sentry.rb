60059f32be5a3cb7da911d50ad4362d36b8b9782
Jeremy Marquis <jeremy@jeremymarquis.com>
2024-11-09 11:51:50 -0500

    sentry only in production

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,6 +3,7 @@
 Sentry.init do |config|
   config.dsn = 'https://c98ff5cabe96311961b7faad358d400b@o4508150432858112.ingest.us.sentry.io/4508150435676160'
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+  config.enabled_environments = ['production']
 
   # Set traces_sample_rate to 1.0 to capture 100%
   # of transactions for tracing.
