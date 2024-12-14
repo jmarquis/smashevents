cd516e1d49842f83e8d4873f82626522b75cd587
Jeremy Marquis <jeremy@jeremymarquis.com>
2024-12-13 21:05:44 -0500

    fix youtube live urls

--- app/lib/youtube.rb
+++ app/lib/youtube.rb
@@ -4,18 +4,16 @@ class Youtube
   class << self
 
     def channel_url(channel_name)
-      Rails.cache.fetch("youtube_channel_url_#{channel_name}", expires_in: Rails.env.development? ? 5.seconds : 7.days) do
-        puts "Fetching YouTube channel info for #{channel_name}..."
+      puts "Fetching YouTube channel info for #{channel_name}..."
 
-        response = StatsD.measure('youtube.list_searches') do
-          client.list_searches('snippet', type: 'channel', q: channel_name)
-        end
-
-        "https://youtube.com/channel/#{response.items.first.id.channel_id}"
-      rescue => e
-        puts e.message
-        "https://youtube.com/#{channel_name}"
+      response = StatsD.measure('youtube.list_searches') do
+        client.list_searches('snippet', type: 'channel', q: channel_name)
       end
+
+      "https://youtube.com/channel/#{response.items.first.id.channel_id}/live"
+    rescue => e
+      puts e.message
+      "https://youtube.com/#{channel_name}"
     end
 
     def client
