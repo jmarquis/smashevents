8e0e39dc355e3766b7a2a77258dd20a55ea23454
Jeremy Marquis <jeremy@jeremymarquis.com>
2025-01-01 12:00:07 -0500

    vernier

--- Gemfile
+++ Gemfile
@@ -87,6 +87,7 @@ gem "dalli", "~> 3.2"
 
 gem "statsd-instrument", "~> 3.9"
 
+gem 'vernier'
 gem "stackprof", "~> 0.2.26"
 gem "sentry-ruby", "~> 5.21"
 gem "sentry-rails", "~> 5.21"
--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -5,16 +5,8 @@ Sentry.init do |config|
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
   config.enabled_environments = ['production']
 
-  # Set traces_sample_rate to 1.0 to capture 100%
-  # of transactions for tracing.
-  # We recommend adjusting this value in production.
-  config.traces_sample_rate = 1.0
-  # or
-  config.traces_sampler = lambda do |context|
-    true
-  end
-  # Set profiles_sample_rate to profile 100%
-  # of sampled transactions.
-  # We recommend adjusting this value in production.
+  config.traces_sample_rate = 0.1
+
+  config.profiler_class = Sentry::Vernier::Profiler
   config.profiles_sample_rate = 1.0
 end
