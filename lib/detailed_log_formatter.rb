32e5a1f240a08be7bf197766a5b44f387d4ed9b4
Jeremy Marquis <jmarquis@etsy.com>
2025-04-04 13:14:11 -0400

    log format cleanup

--- lib/detailed_log_formatter.rb
+++ lib/detailed_log_formatter.rb
@@ -1,5 +1,5 @@
 class DetailedLogFormatter < Logger::Formatter
   def call(severity, time, progname, msg)
-    "[#{severity.upcase}] [#{time.strftime("%Y-%m-%d %H:%M:%S.%L")}] [#{progname}] #{String === msg ? msg : msg.inspect}\n"
+    "[#{severity.upcase}] [#{time.strftime("%Y-%m-%d %H:%M:%S.%L")}]#{progname.present? ? " [#{progname}]" : ''} #{String === msg ? msg : msg.inspect}\n"
   end
 end
