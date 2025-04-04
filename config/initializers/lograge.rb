14e6e58fffe9658a9e13155449f08465ae398746
Jeremy Marquis <jmarquis@etsy.com>
2025-04-04 11:19:55 -0400

    add timestamps to log lines

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,4 +1,9 @@
 Rails.application.configure do
   config.lograge.enabled = true
   config.lograge.ignore_actions = ['ApplicationController#ping']
+  config.lograge.custom_options = lambda do |event|
+    {
+      time: Time.now.utc
+    }
+  end
 end
