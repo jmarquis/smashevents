a8b10a2ca784dffc7fa9d58467a8ae111c793973
Jeremy Marquis <jeremy@jeremymarquis.com>
2025-04-26 10:34:22 -0400

    thread locals

--- lib/detailed_log_formatter.rb
+++ lib/detailed_log_formatter.rb
@@ -3,11 +3,11 @@ class DetailedLogFormatter < Logger::Formatter
   @uuid = nil
 
   def set_entrypoint(entrypoint)
-    @entrypoint = entrypoint
-    @uuid = SecureRandom.uuid_v4
+    Thread.current[:entrypoint] = entrypoint
+    Thread.current[:uuid] = SecureRandom.uuid_v4
   end
 
   def call(severity, time, progname, msg)
-    "[#{severity.upcase}] [#{time.strftime("%Y-%m-%d %H:%M:%S.%L")}]#{progname.present? ? " [#{progname}]" : ''}#{@entrypoint.present? ? " [#{@entrypoint}]" : ''} [#{@uuid}] #{String === msg ? msg : msg.inspect}\n"
+    "[#{severity.upcase}] [#{time.strftime("%Y-%m-%d %H:%M:%S.%L")}]#{progname.present? ? " [#{progname}]" : ''}#{Thread.current[:entrypoint].present? ? " [#{Thread.current[:entrypoint]}]" : ''}#{Thread.current[:uuid].present? ? " [#{Thread.current[:uuid]}]" : ''} #{String === msg ? msg : msg.inspect}\n"
   end
 end
