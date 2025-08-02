cbe1a184f015b4ce892a52dc1376610573298e04
Jeremy Marquis <jeremy@jeremymarquis.com>
2025-08-02 15:27:50 -0400

    👀

--- app/models/event.rb
+++ app/models/event.rb
@@ -382,7 +382,7 @@ class Event < ApplicationRecord
 
     return unless winner_games.present? && loser_games.present? && winner_games > -1 && loser_games > -1
 
-    if upset_factor > 3
+    if upset_factor > 2
       initialize_twitter_upset_thread!
 
       previous_notification = Notification.where(
