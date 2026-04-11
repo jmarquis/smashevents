4a0c3b91d0fe8e9a084b343f4bd46fa03a910e84
Jeremy Marquis <jeremy@jeremymarquis.com>
2026-04-11 19:09:28 -0400

    metrics for api responses

--- app/lib/api.rb
+++ app/lib/api.rb
@@ -1,9 +1,20 @@
 class Api
   class << self
     def instrument(key)
-      StatsD.increment("api.#{self.name.downcase}.#{key}")
-      StatsD.measure("api.#{self.name.downcase}.#{key}") do
-        yield
+      begin
+        StatsD.increment("api.#{self.name.downcase}.#{key}")
+        StatsD.measure("api.#{self.name.downcase}.#{key}") do
+          yield
+        end
+
+        # We can assume it's a success if we get this far in this block.
+        StatsD.increment("api_response.#{self.name.downcase}.#{key}.200")
+      rescue Graphlient::Errors::Error => e
+        status_code = e.try(:status_code)
+        error_key = status_code.present? ? status_code.to_s : "#{e.class.name.demodulize.underscore}"
+        StatsD.increment("api_response.#{self.name.downcase}.#{key}.#{error_key}")
+
+        raise e
       end
     end
   end
