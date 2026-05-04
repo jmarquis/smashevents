d4ebfd6b36cd2df95ad9a35350343ef816f30247
Jeremy Marquis <jeremy@jeremymarquis.com>
2026-05-04 15:32:27 -0400

    fix api stats keys

--- app/lib/api/instrumentable.rb
+++ app/lib/api/instrumentable.rb
@@ -2,19 +2,20 @@ module Api
   module Instrumentable
     def instrument(key)
       begin
-        StatsD.increment("api.#{self.name.downcase}.#{key}")
-        result = StatsD.measure("api.#{self.name.downcase}.#{key}") do
+        class_name = self.name.downcase.demodulize
+        StatsD.increment("api.#{class_name}.#{key}")
+        result = StatsD.measure("api.#{class_name}.#{key}") do
           yield
         end
 
         # We can assume it's a success if we get this far in this block.
-        StatsD.increment("api_response.#{self.name.downcase}.#{key}.200")
+        StatsD.increment("api_response.#{class_name}.#{key}.200")
 
         result
       rescue Graphlient::Errors::Error => e
         status_code = e.try(:status_code)
         error_key = status_code.present? ? status_code.to_s : "#{e.class.name.demodulize.underscore}"
-        StatsD.increment("api_response.#{self.name.downcase}.#{key}.#{error_key}")
+        StatsD.increment("api_response.#{class_name}.#{key}.#{error_key}")
 
         raise e
       end
