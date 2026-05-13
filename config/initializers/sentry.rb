6c22ec7eb4fffb345f9f528522254079bbc63d6e
Jeremy Marquis <jeremy@jeremymarquis.com>
2026-05-13 18:05:12 -0400

    sentry test

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+Rails.logger.info "Initializing Sentry for environment #{Rails.env}"
+
 Sentry.init do |config|
   config.dsn = 'https://28312cd5aa6870d81ebcc66fa1b356d9@o4504436965179392.ingest.us.sentry.io/4511384438374400'
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
@@ -10,3 +12,5 @@ Sentry.init do |config|
   config.profiler_class = Sentry::Vernier::Profiler
   config.profiles_sample_rate = 1.0
 end
+
+Rails.logger.info 'Sentry initialized'
