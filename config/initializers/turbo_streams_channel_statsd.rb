42508c28876f6be1d8661012de58e6a8f07f9173
Jeremy Marquis <jeremy@jeremymarquis.com>
2026-07-09 19:50:09 -0400

    track consumers

--- config/initializers/turbo_streams_channel_statsd.rb
+++ config/initializers/turbo_streams_channel_statsd.rb
@@ -1,5 +1,7 @@
 class Turbo::StreamsChannel
   ACTIVE_SUBSCRIPTIONS = Concurrent::AtomicFixnum.new(0)
+  SUBSCRIPTIONS_PER_CONSUMER = {}
+  SUBSCRIPTIONS_PER_CONSUMER_MUTEX = Mutex.new
 
   after_subscribe :report_subscribed_gauge, unless: :subscription_rejected?
   after_unsubscribe :report_unsubscribed_gauge, unless: :subscription_rejected?
@@ -8,9 +10,26 @@ class Turbo::StreamsChannel
 
   def report_subscribed_gauge
     StatsD.gauge('turbo_streams.subscriptions', ACTIVE_SUBSCRIPTIONS.increment)
+
+    consumer_count = SUBSCRIPTIONS_PER_CONSUMER_MUTEX.synchronize do
+      SUBSCRIPTIONS_PER_CONSUMER[connection] = (SUBSCRIPTIONS_PER_CONSUMER[connection] || 0) + 1
+      SUBSCRIPTIONS_PER_CONSUMER.size
+    end
+    StatsD.gauge('turbo_streams.consumers', consumer_count)
   end
 
   def report_unsubscribed_gauge
     StatsD.gauge('turbo_streams.subscriptions', ACTIVE_SUBSCRIPTIONS.decrement)
+
+    consumer_count = SUBSCRIPTIONS_PER_CONSUMER_MUTEX.synchronize do
+      remaining = (SUBSCRIPTIONS_PER_CONSUMER[connection] || 1) - 1
+      if remaining <= 0
+        SUBSCRIPTIONS_PER_CONSUMER.delete(connection)
+      else
+        SUBSCRIPTIONS_PER_CONSUMER[connection] = remaining
+      end
+      SUBSCRIPTIONS_PER_CONSUMER.size
+    end
+    StatsD.gauge('turbo_streams.consumers', consumer_count)
   end
 end
