a1f1e8d9eac7629f2c01e73abc703c48a0139ff3
Jeremy Marquis <jeremy@jeremymarquis.com>
2026-09-14 15:23:16 -0400

    parrygg stream sync

--- app/lib/ingestor/parrygg.rb
+++ app/lib/ingestor/parrygg.rb
@@ -4,9 +4,30 @@ module Ingestor
 
     class << self
       def sync_streams
-        Rails.logger.info 'Syncing streams'
+        Rails.logger.info 'Syncing streams for parrygg tournaments'
+
         Tournament.not_past.reasonable_duration.where(provider: 'parrygg').each do |tournament|
-          # TODO: Fetch tournament streams
+          next unless tournament.should_display?
+
+          stream_data = Api::Parrygg.tournament_streams(tournament_id: tournament.provider_tournament_id)
+
+          tournament.stream_data = if stream_data.present? && stream_data[:streams].present?
+            stream_data[:streams]&.map do |stream|
+              stream_data = (tournament.stream_data || []).map(&:deep_symbolize_keys).find { |data| data[:name]&.downcase == stream[:channel].downcase } || {}
+
+              stream_data[:name] = stream[:channel]
+              stream_data[:source] = case stream[:platform]
+              when 'STREAM_PLATFORM_TWITCH'
+                Tournament::STREAM_SOURCE_TWITCH
+              when 'STREAM_PLATFORM_YOUTUBE'
+                Tournament::STREAM_SOURCE_YOUTUBE
+              end
+
+              stream_data
+            end
+          end
+
+          tournament.save!
         end
       end
     end
